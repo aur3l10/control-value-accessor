23110685b96f0fcffa11a72d0456906165888c2a
Aurelio <aurelio.arenella@gmail.com>
2020-12-07 23:10:39 +0100

    first custom input

---
+++ src/app/custom-input/custom-input.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: CustomInputComponent,
+    multi: true
+  }]
+})
+export class CustomInputComponent implements ControlValueAccessor {
+
+  value!: string;
+  onChange!: (value: string) => void;
+  onTouched!: () => void;
+  disabled!: boolean;
+
+  constructor() { }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+
+}
